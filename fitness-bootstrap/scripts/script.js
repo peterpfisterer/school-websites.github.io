5e596f2c0660427505836b3735d3312ea7727b5c
Peter Pfisterer <93228159+peterpfisterer@users.noreply.github.com>
2021-11-29 12:27:27 -0700

    Add files via upload

---
+++ fitness-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Peter Pfisterer
+    File Name: script.js
+    Date: 11/29/2021
+*/
+
+//jQuery for hero image to consume header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
