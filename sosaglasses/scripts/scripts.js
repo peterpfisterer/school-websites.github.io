a87188f7fee21524c4843cbd7104b2990a4e47e0
Peter Pfisterer <93228159+peterpfisterer@users.noreply.github.com>
2022-01-21 15:33:42 -0700

    Add files via upload

---
+++ sosaglasses/scripts/scripts.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let userName = "empty";
+//The name variable will be saved for a later release of the website to make it more personal.
+userName =  
+prompt("Please enter your name");
+
+//document.write(`<p>Welcome to my website ${userName}</p>`);
