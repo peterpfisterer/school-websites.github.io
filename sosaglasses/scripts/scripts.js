fc64f50312961538be0f00e5fa557b982ed5f243
Peter Pfisterer <93228159+peterpfisterer@users.noreply.github.com>
2022-01-28 15:32:22 -0700

    Delete sosaglasses directory

--- sosaglasses/scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-let userName = "empty";
-//The name variable will be saved for a later release of the website to make it more personal.
-userName =  
-prompt("Please enter your name");
-
-//document.write(`<p>Welcome to my website ${userName}</p>`);
